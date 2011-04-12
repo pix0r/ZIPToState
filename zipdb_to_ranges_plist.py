3060350d8011c4258528d4493f7ecfc3b4259a5c
Mike Matz <mike@pixor.net>
2011-04-12 09:07:47 -0700

    Simplified plist output format

--- zipdb_to_ranges_plist.py
+++ zipdb_to_ranges_plist.py
@@ -19,53 +19,33 @@ SELECT `ZIP Code`, `State Abbreviation`, `State Name`
 FROM `ZIP Codes`
 INNER JOIN States 
 	ON States.`State Code` = `Zip Codes`.`State Code`
+	ORDER BY `ZIP Code` ASC
 """
 
 conn_in = sqlite3.connect(input_file)
 c_in = conn_in.cursor()
 
 plist = dict(
-		ranges = [],
 		)
 
-def addRange(start, end, state, statecode):
-	print "Adding range", start, end, "for state", state, "(" + statecode + ")"
-	range = dict(
-			start = start,
-			end = end,
-			state = state,
-			code = statecode,
-			)
-	plist["ranges"].append(range)
-
-range_min = 0
-range_max = 0
-last_state = ""
-last_state_code = ""
-total_ranges = 0
-total_zips = 0
-
 c_in.execute(zip_query)
+
+last_state = ""
+total_zips = 0
+total_ranges = 0
 
 for row in c_in:
 	curr_zip = row[0]
 	curr_state_code = row[1]
 	curr_state = row[2]
-
-	if curr_state != last_state:
-		if last_state != "":
-			addRange(range_min, range_max, last_state, last_state_code)
-			total_ranges += 1
-		range_min = curr_zip
-		last_state = curr_state
-		last_state_code = curr_state_code
-	
-	range_max = curr_zip
 	total_zips += 1
-
-if range_max:
-	addRange(range_min, range_max, last_state, last_state_code)
-	total_ranges += 1
+	if curr_state != last_state:
+		plist[curr_zip] = dict(
+				state = curr_state,
+				code = curr_state_code,
+				)
+		last_state = curr_state
+		total_ranges += 1
 
 plistlib.writePlist(plist, output_file)
 
